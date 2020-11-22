33820675302a80b610df6c21e6187d5cb0626cb1
copihuexnegro <fmicheam@gmail.com>
2020-11-22 19:46:32 -0300

    react-helmet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import {BrowserRouter as Router ,Route,Switch}from 'react-router-dom'
 import Principal from './Principal'
-
+import {Helmet} from 'react-helmet'
 function App() { 
   return (
     <div>
+
+        <Helmet>
+          <title>Hotel Ecomusic</title>
+          <meta name="description" content="Hotel Ecomusic , es un hotel ambientado  en la musica internacional y nacional , cada habitacion esta inspirada en un artista de renombre. ademas  en hotel ecomusic creemos fervientemente un pais mas sustentable "/>
+        </Helmet>
 
       <Router>
                 <Switch>
