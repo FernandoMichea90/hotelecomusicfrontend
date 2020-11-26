1add8aec417ae35590973bd322fda113091b8c5f
copihuexnegro <fmicheam@gmail.com>
2020-11-26 01:43:05 -0300

    actualizar descripcion

--- src/App.js
+++ src/App.js
@@ -4,18 +4,15 @@ import Principal from './Principal'
 import {Helmet} from 'react-helmet'
 function App() { 
 
-    const description="Reserva en Hotel  Ecomusic, Valparaíso. Sin gastos de reserva. Excelentes precios,Gran disponibilidad,Bajos Precios,Wifi gratuito, recepción las 24 horas."
-
-
   return (
     <div>
 
         <Helmet>
-          <head>
+         
             <meta charset="utf-8" />
-          <title>Hotel Ecomusic Prueba </title>
+          <title>Hotel Ecomusic </title>
           <meta name="description" content="Reserva en Hotel  Ecomusic, Valparaíso. Sin gastos de reserva. Excelentes precios,Gran disponibilidad,Bajos Precios,Wifi gratuito, recepción las 24 horas."  data-react-helmet="true" />
-          </head>
+          
         </Helmet>
 
       <Router>
